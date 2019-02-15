c7c5c265385da09e9e49a6881ac0962f23ea36c1
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-02-15 15:22:18 -0500

    Incomplete work need test to reflect what result should be

--- moa/reduction.py
+++ moa/reduction.py
@@ -1,8 +1,28 @@
+import itertools
+
+
 from .ast import MOANodeTypes, preorder_replacement
 
 
-def index_reduction(tree):
+def create_index_node(
+
+
+def reduction(tree):
     """Preorder traversal and replacement of ast tree
 
+    In the future the symbol table will have to be constructed earlier
+    for arrays and variables for shapes.
     """
-    pass
+    symbol_table = {}
+    counter = itertools.counter()
+
+    for dimension in tree.shape:
+        symbol_name =
+        symbol_table[
+
+
+    patterns = {
+        (MOANodeTypes.PSI
+
+
+    }
