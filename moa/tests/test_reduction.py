92e3b0039a8425e6abbc65e7e2e5351394038433
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-02-15 16:48:28 -0500

    Added failing test to reduction for lenore example

---
+++ moa/tests/test_reduction.py
@@ -0,0 +1,29 @@
+import pytest
+
+from moa.ast import MOANodeTypes, BinaryNode, UnaryNode, ArrayNode
+from moa.reduction import reduce_ast
+
+
+@pytest.mark.parametrize("tree,result", [
+    # Lenore Simple Example #1 06/01/2018
+    (BinaryNode(MOANodeTypes.PSI, None,
+                ArrayNode(MOANodeTypes.ARRAY, (1,), None, (0,)),
+                UnaryNode(MOANodeTypes.TRANSPOSE, None,
+                          BinaryNode(MOANodeTypes.PLUS, None,
+                                     ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A', None),
+                                     ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B', None)))),
+     ({'i1': (0, 2)},
+      BinaryNode(MOANodeTypes.PLUS, (3,),
+                 BinaryNode(MOANodeTypes.PSI, (3,),
+                            ArrayNode(MOANodeTypes.ARRAY, (1,), None, ('i1', 0)),
+                            ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A', None)),
+                 BinaryNode(MOANodeTypes.PSI, (3,),
+                            ArrayNode(MOANodeTypes.ARRAY, (1,), None, ('i1', 0)),
+                            ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B', None))))),
+])
+def test_reduce_integration(tree, result):
+    result_symbol_table, result_tree = result
+    new_symbol_table, new_tree = reduce_ast(tree)
+    print(new_tree)
+    assert new_symbol_table == result_symbol_table
+    assert new_tree == result_tree
