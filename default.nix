1f504e5344c1d08f13df92a54f071cd22920eb80
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-02-22 14:51:05 -0500

    Update package versions

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
   # pin version
   pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/36f316007494c388df1fec434c1e658542e3c3cc.tar.gz";
-    sha256 = "1w1dg9ankgi59r2mh0jilccz5c4gv30a6q1k6kv2sn8vfjazwp9k";
+    url = "https://github.com/NixOS/nixpkgs/archive/2a81eceeba6d9b0499c0a9dc569921765321cdd0.tar.gz";
+    sha256 = "0zhg87ialczrz4yb4zy104l8wsw45bivpq33qg8czvnsisq77735";
   }) { };
 
   pythonPackages = pkgs.python3Packages;
