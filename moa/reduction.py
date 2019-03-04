9a9bc12a09c3568fb5956b032171e046d65ea362
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-03-04 08:55:32 -0500

    All tests passing for symbolic shapes initial version

--- moa/reduction.py
+++ moa/reduction.py
@@ -3,7 +3,9 @@ import itertools
 from .core import MOAException
 from .ast import (
     MOANodeTypes,
-    ArrayNode, BinaryNode,
+    ArrayNode, BinaryNode, SymbolNode,
+    add_symbol,
+    generate_unique_index_name, generate_unique_array_name,
     preorder_replacement
 )
 from .shape import is_vector, is_scalar
@@ -13,23 +15,24 @@ class MOAReductionError(MOAException):
     pass
 
 
-def add_indexing_node(node, symbol_table, counter):
+def add_indexing_node(symbol_table, node):
     """Adds indexing into the MOA AST
 
     For example: <i0 i1> psi (A + B)
     """
     index_symbols = ()
-    for i, bound in zip(counter, node.shape):
-        symbol_name = f'i{i}'
-        symbol_table[symbol_name] = (0, bound)
-        index_symbols = index_symbols + (symbol_name,)
+    for bound in node.shape:
+        index_name = generate_unique_index_name(symbol_table)
+        symbol_table = add_symbol(symbol_table, index_name, MOANodeTypes.INDEX, (), (0, bound))
+        index_symbols = index_symbols + (index_name,)
 
-    return BinaryNode(MOANodeTypes.PSI, node.shape,
-                      ArrayNode(MOANodeTypes.ARRAY, (len(index_symbols),), None, index_symbols),
-                      node)
+    array_name = generate_unique_array_name(symbol_table)
+    symbol_table = add_symbol(symbol_table, array_name, MOANodeTypes.ARRAY, (len(index_symbols),), index_symbols)
+    vector_node = ArrayNode(MOANodeTypes.ARRAY, (len(index_symbols),), array_name)
+    return symbol_table, BinaryNode(MOANodeTypes.PSI, node.shape, vector_node, node)
 
 
-def reduce_ast(tree, max_iterations=100):
+def reduce_ast(symbol_table, tree, max_iterations=100):
     """Preorder traversal and replacement of ast tree
 
     In the future the symbol table will have to be constructed earlier
@@ -37,15 +40,12 @@ def reduce_ast(tree, max_iterations=100):
 
     TODO: change exception to warning to allow for partial replacement
     """
-    counter = itertools.count()
-    symbol_table = {}
-
-    tree = add_indexing_node(tree, symbol_table, counter)
-    tree = preorder_replacement(tree, _reduce_replacement, range(max_iterations))
+    symbol_table, tree = add_indexing_node(symbol_table, tree)
+    symbol_table, tree = preorder_replacement(symbol_table, tree, _reduce_replacement, range(max_iterations))
     return symbol_table, tree
 
 
-def _reduce_replacement(node):
+def _reduce_replacement(symbol_table, node):
     reduce_psi_map = {
         MOANodeTypes.PSI: _reduce_psi_psi,
         MOANodeTypes.TRANSPOSE: _reduce_psi_transpose,
@@ -59,35 +59,40 @@ def _reduce_replacement(node):
 
     # Is reduction as simple as exclusively looking at psi nodes?
     if node.node_type == MOANodeTypes.PSI and node.right_node.node_type in reduce_psi_map:
-        if not is_vector(node.left_node) or node.left_node.value is None:
+        if not is_vector(symbol_table, node.left_node) or symbol_table[node.left_node.symbol_node].value is None:
             raise MOAReductionError('PSI replacement assumes that left_node is vector with defined values')
-        return reduce_psi_map[node.right_node.node_type](node)
-    return None
+        return reduce_psi_map[node.right_node.node_type](symbol_table, node)
+    return None, None
 
 
-def _reduce_psi_psi(node):
+def _reduce_psi_psi(symbol_table, node):
     """<i j> psi <k l> psi ... => <k l i j> psi ..."""
-    if not is_vector(node.right_node.left_node) or node.right_node.left_node.value is None:
+    if not is_vector(symbol_table, node.right_node.left_node) or symbol_table[node.right_node.left_node.symbol_node].value is None:
         raise MOAReductionError('<...> PSI <...> PSI ... replacement assumes that the inner left_node is vector with defined values')
 
-    index_values = node.right_node.left_node.value + node.left_node.value
-    return BinaryNode(MOANodeTypes.PSI, node.shape,
-                      ArrayNode(MOANodeTypes.ARRAY, (len(index_values),), None, index_values),
-                      node.right_node.right_node)
+    array_name = generate_unique_array_name(symbol_table)
+    array_values = symbol_table[node.right_node.left_node.symbol_node].value + symbol_table[node.left_node.symbol_node].value
+    symbol_table = add_symbol(symbol_table, array_name, MOANodeTypes.ARRAY, (len(array_values),), array_values)
+
+    return symbol_table, BinaryNode(MOANodeTypes.PSI, node.shape,
+                                    ArrayNode(MOANodeTypes.ARRAY, (len(array_values),), array_name),
+                                    node.right_node.right_node)
 
 
-def _reduce_psi_transpose(node):
+def _reduce_psi_transpose(symbol_table, node):
     """<i j k> psi transpose ... => <k j i> psi ..."""
-    index_values = node.left_node.value[::-1]
-    return BinaryNode(MOANodeTypes.PSI, node.shape,
-                      ArrayNode(MOANodeTypes.ARRAY, (len(index_values),), None, index_values),
-                      node.right_node.right_node)
+    array_name = generate_unique_array_name(symbol_table)
+    array_values = symbol_table[node.left_node.symbol_node].value[::-1]
+    symbol_table = add_symbol(symbol_table, array_name, MOANodeTypes.ARRAY, (len(array_values),), array_values)
+
+    return symbol_table, BinaryNode(MOANodeTypes.PSI, node.shape,
+                                    ArrayNode(MOANodeTypes.ARRAY, (len(array_values),), array_name),
+                                    node.right_node.right_node)
 
 
-def _reduce_psi_transposev(node):
+def _reduce_psi_transposev(symbol_table, node):
     """<i j k> psi <2 0 1> transpose ... => <k i j> psi ..."""
-    # sort two lists according to one list
-    index_values = tuple(s for _, s in sorted(zip(node.right_node.left_node.value, node.left_node.value), key=lambda pair: pair[0]))
+    array_values = tuple(s for _, s in sorted(zip(node.right_node.left_node.value, node.left_node.value), key=lambda pair: pair[0]))
     return BinaryNode(MOANodeTypes.PSI, node.shape,
                       ArrayNode(MOANodeTypes.ARRAY, (len(index_values),), None, index_values),
                       node.right_node.right_node)
@@ -97,24 +102,24 @@ def _reduce_psi_plus_red(node):
     raise NotImplemenetedError('PSI +RED')
 
 
-def _reduce_psi_plus_minus_times_divide(node):
+def _reduce_psi_plus_minus_times_divide(symbol_table, node):
     """<i j> psi (... (+-*/) ...) => (<i j> psi ...) (+-*/) (<k l> psi ...)
 
     Scalar Extension
       <i j> psi (scalar (+-*/) ...) = scalar (+-*/) <i j> psi ...
     """
-    if is_scalar(node.right_node.left_node):
+    if is_scalar(symbol_table, node.right_node.left_node):
         left_node = node.right_node.left_node
     else:
         left_node = BinaryNode(MOANodeTypes.PSI, node.shape,
                                node.left_node,
                                node.right_node.left_node)
 
-    if is_scalar(node.right_node.right_node):
+    if is_scalar(symbol_table, node.right_node.right_node):
         right_node = node.right_node.right_node
     else:
         right_node = BinaryNode(MOANodeTypes.PSI, node.shape,
                                 node.left_node,
                                 node.right_node.right_node)
 
-    return BinaryNode(node.right_node.node_type, node.shape, left_node, right_node)
+    return symbol_table, BinaryNode(node.right_node.node_type, node.shape, left_node, right_node)
--- tests/backend/test_python.py
+++ tests/backend/test_python.py
@@ -3,51 +3,63 @@ import ast
 import astunparse
 import pytest
 
-from moa.ast import BinaryNode, UnaryNode, ArrayNode, MOANodeTypes
+from moa.ast import MOANodeTypes, BinaryNode, UnaryNode, ArrayNode, SymbolNode
 from moa.backend import python_backend
 
 
-@pytest.mark.parametrize('tree,result', [
-    (BinaryNode(MOANodeTypes.PLUS, (3, 4),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A', None),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B', None)),
+@pytest.mark.parametrize('symbol_table, tree, expected_source', [
+    ({'A': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None),
+      'B': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None)},
+     BinaryNode(MOANodeTypes.PLUS, (3, 4),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A'),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B')),
      '(A + B)'
     ),
-    (BinaryNode(MOANodeTypes.MINUS, (3, 4),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A', None),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B', None)),
+    ({'A': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None),
+      'B': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None)},
+     BinaryNode(MOANodeTypes.MINUS, (3, 4),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A'),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B')),
      '(A - B)'
     ),
-    (BinaryNode(MOANodeTypes.TIMES, (3, 4),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A', None),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B', None)),
+    ({'A': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None),
+      'B': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None)},
+     BinaryNode(MOANodeTypes.TIMES, (3, 4),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A'),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B')),
      '(A * B)'
     ),
-    (BinaryNode(MOANodeTypes.DIVIDE, (3, 4),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A', None),
-                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B', None)),
+    ({'A': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None),
+      'B': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None)},
+     BinaryNode(MOANodeTypes.DIVIDE, (3, 4),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A'),
+                ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B')),
      '(A / B)'
     ),
 ])
-def test_python_backend_unit(tree, result):
-    ast = python_backend(tree)
+def test_python_backend_unit(symbol_table, tree, expected_source):
+    ast = python_backend(symbol_table, tree)
     source = astunparse.unparse(ast)[:-1] # remove newline
-    assert source == result
+    assert source == expected_source
 
 
-@pytest.mark.parametrize('tree,result', [
+@pytest.mark.parametrize('symbol_table, tree, expected_source', [
     # Lenore Simple Example #1 06/01/2018
-    (BinaryNode(MOANodeTypes.PLUS, (3,),
+    ({'A': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None),
+      'B': SymbolNode(MOANodeTypes.ARRAY, (3, 4), None),
+      '_i3': SymbolNode(MOANodeTypes.INDEX, (), (0, 3)),
+      '_a4': SymbolNode(MOANodeTypes.ARRAY, shape=(2,), value=('_i3', 0))},
+     BinaryNode(MOANodeTypes.PLUS, (3,),
                 BinaryNode(MOANodeTypes.PSI, (3,),
-                           ArrayNode(MOANodeTypes.ARRAY, (2,), None, ('i0', 0)),
-                           ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A', None)),
+                           ArrayNode(MOANodeTypes.ARRAY, (2,), '_a4'),
+                           ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'A')),
                 BinaryNode(MOANodeTypes.PSI, (3,),
-                           ArrayNode(MOANodeTypes.ARRAY, (2,), None, ('i0', 0)),
-                           ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B', None))),
-     "(A[('i0', 0)] + B[('i0', 0)])"
+                           ArrayNode(MOANodeTypes.ARRAY, (2,), '_a4'),
+                           ArrayNode(MOANodeTypes.ARRAY, (3, 4), 'B'))),
+     "(A[('_i3', 0)] + B[('_i3', 0)])"
     )
 ])
-def test_python_backend_integration(tree, result):
-    ast = python_backend(tree)
+def test_python_backend_integration(symbol_table, tree, expected_source):
+    ast = python_backend(symbol_table, tree)
     source = astunparse.unparse(ast)[:-1] # remove newline
-    assert source == result
+    assert source == expected_source
