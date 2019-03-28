28905a919423120fd9eab5567655c176644b2708
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-03-28 13:30:55 -0400

    Working on getting frontend back to working

--- moa/frontend/array.py
+++ moa/frontend/array.py
@@ -1,21 +1,13 @@
-from ..ast import (
-    MOANodeTypes,
-    Node,
-    add_symbol, generate_unique_array_name,
-    join_symbol_tables
-)
-from .. import compiler
+from .. import ast
+# from .. import compiler
 from ..shape import calculate_shapes
-from ..dnf import reduce_to_dnf
-from ..onf import reduce_to_onf
-from ..analysis import metric_flops
+# from ..dnf import reduce_to_dnf
+# from ..onf import reduce_to_onf
+# from ..analysis import metric_flops
 from ..visualize import visualize_ast, print_ast
 
 class LazyArray:
     def __init__(self, shape, value=None, name=None):
-        self.symbol_table = {}
-        self.tree = None
-
         if name is None and value is None:
             raise ValueError('either name or value must be supplied for LazyArray')
 
@@ -23,9 +15,10 @@ class LazyArray:
             raise NotImplemenetedError('not able to pass in value at compile time at this moment')
 
         shape = self._create_array_from_list_tuple(shape)
-        name = name or generate_unique_array_name(self.symbol_table)
-        self.symbol_table = add_symbol(self.symbol_table, name, MOANodeTypes.ARRAY, shape, value)
-        self.tree = Node(MOANodeTypes.ARRAY, None, name)
+        name = name or ast.generate_unique_array_name(self.context)
+
+        self.context = ast.create_context(ast=ast.Node((ast.NodeSymbol.ARRAY,), None, (name,), ()))
+        self.context = ast.add_symbol(self.context, name, ast.NodeSymbol.ARRAY, shape, None, value)
 
     def __getitem__(self, index):
         if isinstance(index, (int, str, slice)):
@@ -37,8 +30,8 @@ class LazyArray:
             if isinstance(i, str):
                 if strides:
                     raise IndexError('current limitation that indexing is not allowed past strides')
-                self.symbol_table = add_symbol(self.symbol_table, i, MOANodeTypes.ARRAY, (), None)
-                indicies = indicies + (Node(MOANodeTypes.ARRAY, (), i),)
+                self.context = ast.add_symbol(self.context, i, ast.NodeSymbol.ARRAY, (), None, None)
+                indicies = indicies + (ast.Node((ast.NodeSymbol.ARRAY,), (), (i,), ()),)
             elif isinstance(i, int):
                 if strides:
                     raise IndexError('current limitation that indexing is not allowed past strides')
@@ -49,11 +42,11 @@ class LazyArray:
                 raise IndexError('only integers, symbols, and slices (`:`) are valid indicies')
 
         if indicies:
-            array_name = generate_unique_array_name(self.symbol_table)
-            self.symbol_table = add_symbol(self.symbol_table, array_name, MOANodeTypes.ARRAY, (len(indicies),), indicies)
-            self.tree = Node(MOANodeTypes.PSI, None,
-                                   Node(MOANodeTypes.ARRAY, None, array_name),
-                                   self.tree)
+            array_name = ast.generate_unique_array_name(self.context)
+            self.context = ast.add_symbol(self.context, array_name, NodeSymbol.ARRAY, (len(indicies),), indicies)
+            self.context = ast.replace_node(
+                ast.Node(ast.NodeSymbol.PSI, None, (), (ast.Node((NodeSymbol.ARRAY,), None, array_name), None)),
+                self.context.ast, (1,))
         if strides:
             raise NotImplemenetedError('strides not implemented')
         return self
@@ -61,11 +54,11 @@ class LazyArray:
     def _create_array_from_int_float_string(self, value):
         if isinstance(value, str):
             array_name = value
-            self.symbol_table = add_symbol(self.symbol_table, array_name, MOANodeTypes.ARRAY, (), None)
+            self.context = ast.add_symbol(self.context, array_name, NodeSymbol.ARRAY, (), None, None)
         else:
-            array_name = generate_unique_array_name(self.symbol_table)
-            self.symbol_table = add_symbol(self.symbol_table, array_name, MOANodeTypes.ARRAY, (), (value,))
-        return Node(MOANodeTypes.ARRAY, None, array_name)
+            array_name = ast.generate_unique_array_name(self.context)
+            self.context = add_symbol(self.context, array_name, NodeSymbol.ARRAY, (), None, (value,))
+        return ast.Node((ast.NodeSymbol.ARRAY,), None, (array_name,), ())
 
     def _create_array_from_list_tuple(self, value):
         if not any(isinstance(_, str) for _ in value):
@@ -74,8 +67,8 @@ class LazyArray:
         elements = ()
         for element in value:
             if isinstance(element, str):
-                self.symbol_table = add_symbol(self.symbol_table, element, MOANodeTypes.ARRAY, (), None)
-                elements = elements + (Node(MOANodeTypes.ARRAY, None, element),)
+                self.context = ast.add_symbol(self.context, element, NodeSymbol.ARRAY, (), None, None)
+                elements = elements + (ast.Node((ast.NodeSymbol.ARRAY,), None, (element,), ()),)
             else:
                 elements = elements + (element,)
         return elements
@@ -86,44 +79,46 @@ class LazyArray:
 
     def transpose(self, transpose_vector=None):
         if transpose_vector is None:
-            self.tree = Node(MOANodeTypes.TRANSPOSE, None, self.tree)
+            self.context = ast.replace_node(
+                ast.Node((ast.NodeSymbol.TRANSPOSE,), None, (), (None,)),
+                self.context.ast, (0,))
         else:
             symbolic_vector = self._create_array_from_list_tuple(transpose_vector)
 
-            array_name = generate_unique_array_name(self.symbol_table)
-            self.symbol_table = add_symbol(self.symbol_table, array_name, MOANodeTypes.ARRAY, (len(symbolic_vector),), tuple(symbolic_vector))
-            self.tree = Node(MOANodeTypes.TRANSPOSEV, None,
-                                   Node(MOANodeTypes.ARRAY, None, array_name),
-                                   self.tree)
+            array_name = generate_unique_array_name(self.context)
+            self.context = ast.add_symbol(self.context, array_name, NodeSymbol.ARRAY, (len(symbolic_vector),), None, tuple(symbolic_vector))
+            self.context = ast.replace_node(
+                ast.Node((ast.NodeSymbol.TRANSPOSEV,), None, (), (ast.Node((NodeSymbol.ARRAY,), None, (array_name,), ()), None)),
+                self.context.ast, (1,))
         return self
 
-    def outer(self, operation, array):
-        operation_map = {
-            '+': (MOANodeTypes.DOT, MOANodeTypes.PLUS),
-            '-': (MOANodeTypes.DOT, MOANodeTypes.MINUS),
-            '*': (MOANodeTypes.DOT, MOANodeTypes.TIMES),
-            '/': (MOANodeTypes.DOT, MOANodeTypes.DIVIDE),
-        }
-        if operation not in operation:
-            raise ValueError(f'operation {operation} not in allowed operations (+-*/)')
+    # def outer(self, operation, array):
+    #     operation_map = {
+    #         '+': (NodeSymbol.DOT, NodeSymbol.PLUS),
+    #         '-': (NodeSymbol.DOT, NodeSymbol.MINUS),
+    #         '*': (NodeSymbol.DOT, NodeSymbol.TIMES),
+    #         '/': (NodeSymbol.DOT, NodeSymbol.DIVIDE),
+    #     }
+    #     if operation not in operation:
+    #         raise ValueError(f'operation {operation} not in allowed operations (+-*/)')
 
-        if isinstance(array, self.__class__):
-            self.symbol_table, left_tree, right_tree = join_symbol_tables(self.symbol_table, self.tree, array.symbol_table, array.tree)
-            self.tree = Node(operation_map[operation], None, left_tree, right_tree)
-        elif isinstance(left, (int, float, str)):
-            self.tree = Node(operation_map[operation], None,
-                                   self.tree,
-                                   self._create_array_from_int_float_string(array))
-        else:
-            raise TypeError(f'not known how to handle outer product with type {type(array)}')
-        return self
+    #     if isinstance(array, self.__class__):
+    #         self.symbol_table, left_tree, right_tree = join_symbol_tables(self.symbol_table, self.tree, array.symbol_table, array.tree)
+    #         self.tree = Node(operation_map[operation], None, left_tree, right_tree)
+    #     elif isinstance(left, (int, float, str)):
+    #         self.tree = Node(operation_map[operation], None,
+    #                                self.tree,
+    #                                self._create_array_from_int_float_string(array))
+    #     else:
+    #         raise TypeError(f'not known how to handle outer product with type {type(array)}')
+    #     return self
 
     def reduce(self, operation):
         operation_map = {
-            '+': (MOANodeTypes.REDUCE, MOANodeTypes.PLUS),
-            '-': (MOANodeTypes.REDUCE, MOANodeTypes.MINUS),
-            '*': (MOANodeTypes.REDUCE, MOANodeTypes.TIMES),
-            '/': (MOANodeTypes.REDUCE, MOANodeTypes.DIVIDE),
+            '+': (NodeSymbol.REDUCE, NodeSymbol.PLUS),
+            '-': (NodeSymbol.REDUCE, NodeSymbol.MINUS),
+            '*': (NodeSymbol.REDUCE, NodeSymbol.TIMES),
+            '/': (NodeSymbol.REDUCE, NodeSymbol.DIVIDE),
         }
         if operation not in operation:
             raise ValueError(f'operation {operation} not in allowed operations (+-*/)')
@@ -131,70 +126,70 @@ class LazyArray:
         self.tree = Node(operation_map[operation], None, None, self.tree)
         return self
 
-    def _rbinary_opperation(self, operation, left):
-        if isinstance(left, self.__class__):
-            self.symbol_table, left_tree, right_tree = join_symbol_tables(left.symbol_table, left.tree, self.symbol_table, self.tree)
-            self.tree = Node(operation, None, left_tree, right_tree)
-        elif isinstance(left, (int, float, str)):
-            self.tree = Node(operation, None, self._create_array_from_int_float_string(left), self.tree)
-        else:
-            raise TypeError(f'not known how to handle binary operation with type {type(left)}')
-        return self
+    # def _rbinary_opperation(self, operation, left):
+    #     if isinstance(left, self.__class__):
+    #         self.symbol_table, left_tree, right_tree = join_symbol_tables(left.symbol_table, left.tree, self.symbol_table, self.tree)
+    #         self.tree = Node(operation, None, left_tree, right_tree)
+    #     elif isinstance(left, (int, float, str)):
+    #         self.tree = Node(operation, None, self._create_array_from_int_float_string(left), self.tree)
+    #     else:
+    #         raise TypeError(f'not known how to handle binary operation with type {type(left)}')
+    #     return self
 
-    def _binary_opperation(self, operation, right):
-        if isinstance(right, self.__class__):
-            self.symbol_table, left_tree, right_tree = join_symbol_tables(self.symbol_table, self.tree, right.symbol_table, right.tree)
-            self.tree = Node(operation, None, left_tree, right_tree)
-        elif isinstance(right, (int, float, str)):
-            self.tree = Node(operation, None, self.tree, self._create_array_from_int_float_string(right))
-        else:
-            raise TypeError(f'not known how to handle binary operation with type {type(right)}')
-        return self
+    # def _binary_opperation(self, operation, right):
+    #     if isinstance(right, self.__class__):
+    #         self.symbol_table, left_tree, right_tree = join_symbol_tables(self.symbol_table, self.tree, right.symbol_table, right.tree)
+    #         self.tree = Node(operation, None, left_tree, right_tree)
+    #     elif isinstance(right, (int, float, str)):
+    #         self.tree = Node(operation, None, self.tree, self._create_array_from_int_float_string(right))
+    #     else:
+    #         raise TypeError(f'not known how to handle binary operation with type {type(right)}')
+    #     return self
 
-    def __add__(self, other):
-        return self._binary_opperation(MOANodeTypes.PLUS, other)
+    # def __add__(self, other):
+    #     return self._binary_opperation(NodeSymbol.PLUS, other)
 
-    def __radd__(self, other):
-        return self._rbinary_opperation(MOANodeTypes.PLUS, other)
+    # def __radd__(self, other):
+    #     return self._rbinary_opperation(NodeSymbol.PLUS, other)
 
-    def __sub__(self, other):
-        return self._binary_opperation(MOANodeTypes.MINUS, other)
+    # def __sub__(self, other):
+    #     return self._binary_opperation(NodeSymbol.MINUS, other)
 
-    def __rsub__(self, other):
-        return self._rbinary_opperation(MOANodeTypes.MINUS, other)
+    # def __rsub__(self, other):
+    #     return self._rbinary_opperation(NodeSymbol.MINUS, other)
 
-    def __mul__(self, other):
-        return self._binary_opperation(MOANodeTypes.TIMES, other)
+    # def __mul__(self, other):
+    #     return self._binary_opperation(NodeSymbol.TIMES, other)
 
-    def __rmul__(self, other):
-        return self._rbinary_opperation(MOANodeTypes.TIMES, other)
+    # def __rmul__(self, other):
+    #     return self._rbinary_opperation(NodeSymbol.TIMES, other)
 
-    def __truediv__(self, other):
-        return self._binary_opperation(MOANodeTypes.DIVIDE, other)
+    # def __truediv__(self, other):
+    #     return self._binary_opperation(NodeSymbol.DIVIDE, other)
 
-    def __rtruediv__(self, other):
-        return self._rbinary_opperation(MOANodeTypes.DIVIDE, other)
+    # def __rtruediv__(self, other):
+    #     return self._rbinary_opperation(NodeSymbol.DIVIDE, other)
 
-    def compile(self, backend='python', **kwargs):
-        return compiler.compiler(self, frontend='array', backend=backend, **kwargs)
+    # def compile(self, backend='python', **kwargs):
+    #     return compiler.compiler(self, frontend='array', backend=backend, **kwargs)
 
     def _shape(self):
         return calculate_shapes(self.symbol_table, self.tree)
 
-    def _dnf(self):
-        return reduce_to_dnf(*self._shape())
+    # def _dnf(self):
+    #     return reduce_to_dnf(*self._shape())
 
-    def _onf(self):
-        return reduce_to_onf(*self._dnf())
+    # def _onf(self):
+    #     return reduce_to_onf(*self._dnf())
 
-    def analysis(self):
-        shape_symbol_table, shape_tree = calculate_shapes(self.symbol_table, self.tree)
-        dnf_symbol_table, dnf_tree = reduce_to_dnf(shape_symbol_table, shape_tree)
+    # def analysis(self):
+    #     shape_symbol_table, shape_tree = calculate_shapes(self.symbol_table, self.tree)
+    #     dnf_symbol_table, dnf_tree = reduce_to_dnf(shape_symbol_table, shape_tree)
 
-        return {
-            'unoptimized_flops': metric_flops(shape_symbol_table, shape_tree),
-            'optimized_flops': metric_flops(dnf_symbol_table, dnf_tree)
-        }
+    #     return {
+    #         'unoptimized_flops': metric_flops(shape_symbol_table, shape_tree),
+    #         'optimized_flops': metric_flops(dnf_symbol_table, dnf_tree)
+    #     }
 
     def visualize(self, stage=None, as_text=False):
         if stage not in {None, 'shape', 'dnf', 'onf'}:
