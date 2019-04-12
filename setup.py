18b2116c4c5faf2da1227072e2e569945048f703
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-04-12 08:25:49 -0400

    setup.py cleanup inspired by uarray

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup, find_packages
 from os import path
-import io
 
 here = path.abspath(path.dirname(__file__))
-with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='python-moa',
     version='0.5.0',
+    python_requires='>=3.6',
     description='Python Mathematics of Arrays (MOA)',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/Quansight-Labs/python-moa',
-    author='Quansight',
-    author_email='costrouchov@quansight.com',
+    keywords='tensor,compiler,moa',
     maintainer='Christopher Ostrouchov',
     maintainer_email='costrouchov@quansight.com',
+    license='BSD 3-Clause License (Revised)',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         "License :: OSI Approved :: BSD License",
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
     ],
-    keywords='mathematics compiler moa',
     packages=find_packages(exclude=['docs', 'tests', 'notebooks', 'benchmarks']),
-    python_requires='>=3.6',
     install_requires=['sly', 'astunparse'],
     extras_require={
         'viz': ['graphviz'],
